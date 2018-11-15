708c3e7d3ae46dc711d2c8d6f0725a3d6106fc57
Brian Vestergaard Danielsen <bvd@spiir.dk>
2018-11-15 12:51:00 +0100

    Added transactions view controller

---
+++ ios/QuickSprout/QuickSprout/Model/QSTransaction.swift
@@ -0,0 +1,9 @@
+//
+//  QSTransaction.swift
+//  QuickSprout
+//
+//  Created by Brian Vestergaard Danielsen on 14/11/2018.
+//  Copyright © 2018 Brian Vestergaard Danielsen. All rights reserved.
+//
+
+import Foundation
---
+++ ios/QuickSprout/QuickSprout/Views/QSTransactionTableViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  QSTransactionTableViewCell.swift
+//  QuickSprout
+//
+//  Created by Brian Vestergaard Danielsen on 30/10/2018.
+//  Copyright © 2018 Brian Vestergaard Danielsen. All rights reserved.
+//
+
+import UIKit
+
+class QSTransactionTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var currency: UILabel!
+    @IBOutlet weak var amount: UILabel!
+    @IBOutlet weak var type: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state
+    }
+    
+    func populate(transaction: QSTransaction) -> Void {
+        name.text = transaction.text
+        date.text = transaction.date
+        currency.text = transaction.currency
+        if let _amount = transaction.amount {
+            amount.text = String(format:"%.2f", _amount)
+        }
+        type.text = transaction.type
+    }
+}
