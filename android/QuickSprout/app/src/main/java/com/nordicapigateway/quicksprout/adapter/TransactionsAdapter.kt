e57bd310f289910b0cb536a6f59af5d8cfdf199c
Christian Dias Løgberg <cdl@spiir.dk>
2019-06-11 15:29:20 +0200

    Map transactions V2

--- android/QuickSprout/app/src/main/java/com/nordicapigateway/quicksprout/adapter/TransactionsAdapter.kt
+++ android/QuickSprout/app/src/main/java/com/nordicapigateway/quicksprout/adapter/TransactionsAdapter.kt
@@ -22,11 +22,15 @@ class TransactionsAdapter(private val data: JSONArray) : RecyclerView.Adapter<Tr
     }
 
     override fun onBindViewHolder(holder: TransactionsViewHolder, position: Int) {
-        holder.container.type.text = (data[position] as JSONObject).get("type").toString()
-        holder.container.text.text = (data[position] as JSONObject).get("text").toString()
-        holder.container.date.text = (data[position] as JSONObject).get("date").toString()
-        holder.container.currency.text =  (data[position] as JSONObject).get("amount").toString() + (data[position] as JSONObject).get("currency").toString()
-        holder.container.state.text = (data[position] as JSONObject).get("state").toString()
+        val transaction = data[position] as JSONObject
+        holder.container.type.text = transaction.get("type").toString()
+        holder.container.text.text = transaction.get("text").toString()
+        holder.container.date.text = transaction.get("date").toString()
+        val amount = transaction.get("amount") as JSONObject
+        holder.container.amountView.text =  amount.get("value").toString() + " " + amount.get("currency").toString()
+        val balance = transaction.get("balance") as JSONObject
+        holder.container.balanceView.text =  balance.get("value").toString() + " " + balance.get("currency").toString()
+        holder.container.state.text = transaction.get("state").toString()
     }
 
     override fun getItemCount() = data.length()
