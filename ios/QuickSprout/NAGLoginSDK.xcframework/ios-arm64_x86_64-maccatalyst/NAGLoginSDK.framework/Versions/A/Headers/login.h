e36ee0708cc43716446dc60cabc681901b870059
Jesper Johansen <jesper@gundlund.com>
2021-01-25 16:03:19 +0100

    Use XCFramework for SDK
    Now using .XCFramework instead .Framework.

---
+++ ios/QuickSprout/NAGLoginSDK.xcframework/ios-arm64_x86_64-maccatalyst/NAGLoginSDK.framework/Versions/A/Headers/login.h
@@ -0,0 +1,19 @@
+//
+//  login.h
+//  login
+//
+//  Created by Brian Vestergaard Danielsen on 07/11/2018.
+//  Copyright © 2018 Nordic API Gateway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for login.
+FOUNDATION_EXPORT double loginVersionNumber;
+
+//! Project version string for login.
+FOUNDATION_EXPORT const unsigned char loginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <login/PublicHeader.h>
+
+
